80d455af418e555af8bfe54fe819861fb1ffde76
Straple <hadayegor@gmail.com>
2024-11-19 15:42:49 +0300

    update Position

--- Solution/Objects/position.cpp
+++ Solution/Objects/position.cpp
@@ -1,36 +1,52 @@
 #include "position.hpp"
 
 #include "assert.hpp"
-#include "environment.hpp"
+#include "map.hpp"
 
-Position::Position(int pos, int dir) : pos(pos), x(pos / get_env().get_cols()), y(pos % get_env().get_cols()),
-                                       dir(dir) {
+Position::Position(uint32_t pos, uint32_t dir)
+    : x(pos / get_map().get_cols()),
+      y(pos % get_map().get_cols()),
+      dir(dir) {
+    ASSERT(0 <= dir && dir < 4, "invalid dir");
+    ASSERT(0 <= pos < get_map().get_size(), "invalid pos");
+    ASSERT(0 <= x && x < get_map().get_cols(), "invalid x");
+    ASSERT(0 <= y && y < get_map().get_cols(), "invalid y");
+}
+
+uint32_t Position::get_pos() const {
+    return x * get_map().get_cols() + y;
+}
+
+uint32_t Position::get_x() const {
+    return x;
+}
+
+uint32_t Position::get_y() const {
+    return y;
+}
+
+uint32_t Position::get_dir() const {
+    return dir;
 }
 
 bool Position::is_valid() const {
-    ASSERT(0 <= dir && dir < 4, "invalid position dir: " + std::to_string(dir));
-    return 0 <= x && x < get_env().get_rows() &&
-           0 <= y && y < get_env().get_cols() &&
-           0 <= pos && pos < get_env().get_rows() * get_env().get_cols() &&
-           get_env().is_free(pos);
+    ASSERT(0 <= dir && dir < 4, "invalid position dir");
+    return 0 <= x && x < get_map().get_rows() &&
+           0 <= y && y < get_map().get_cols() &&
+           get_map().is_free(get_pos());
 }
 
 Position Position::move_forward() const {
     Position p = *this;
+    ASSERT(0 <= dir && dir < 4, "invalid position dir");
     if (dir == 0) {
         p.y++;
-        p.pos++;
     } else if (dir == 1) {
-        p.pos += get_env().get_cols();
         p.x++;
     } else if (dir == 2) {
-        p.pos--;
         p.y--;
     } else if (dir == 3) {
-        p.pos -= get_env().get_cols();
         p.x--;
-    } else {
-        FAILED_ASSERT("invalid position dir: " + std::to_string(dir));
     }
     return p;
 }
@@ -38,16 +54,18 @@ Position Position::move_forward() const {
 Position Position::rotate() const {
     Position p = *this;
     p.dir = (p.dir + 1) % 4;
+    ASSERT(0 <= p.dir && p.dir < 4, "invalid position dir");
     return p;
 }
 
 Position Position::counter_rotate() const {
     Position p = *this;
     p.dir = (p.dir - 1 + 4) % 4;
+    ASSERT(0 <= p.dir && p.dir < 4, "invalid position dir");
     return p;
 }
 
-[[nodiscard]] Position Position::simulate_action(Action action) const {
+Position Position::simulate_action(const Action &action) const {
     if (action == Action::FW) {
         return move_forward();
     } else if (action == Action::CR) {
@@ -63,7 +81,6 @@ Position Position::counter_rotate() const {
 bool operator==(const Position &lhs, const Position &rhs) {
     return lhs.x == rhs.x &&
            lhs.y == rhs.y &&
-           lhs.pos == rhs.pos &&
            lhs.dir == rhs.dir;
 }
 
@@ -72,8 +89,11 @@ bool operator!=(const Position &lhs, const Position &rhs) {
 }
 
 bool operator<(const Position &lhs, const Position &rhs) {
-    if (lhs.pos != rhs.pos) {
-        return lhs.pos < rhs.pos;
+    if (lhs.x != rhs.x) {
+        return lhs.x < rhs.x;
+    }
+    if (lhs.y != rhs.y) {
+        return lhs.y < rhs.y;
     }
     return lhs.dir < rhs.dir;
 }
--- Solution/Objects/position.hpp
+++ Solution/Objects/position.hpp
@@ -2,14 +2,22 @@
 
 #include "ActionModel.h"
 
-struct Position {
-    int pos = 0;// = x * cols + y
-    int x = 0, y = 0;
-    int dir = 0;// 0:east, 1:south, 2:west, 3:north
+class Position {
+    uint16_t x = 0, y = 0;
+    uint16_t dir = 0;// 0:east, 1:south, 2:west, 3:north
 
+public:
     Position() = default;
 
-    Position(int pos, int dir);
+    Position(uint32_t pos, uint32_t dir);
+
+    [[nodiscard]] uint32_t get_pos() const;
+
+    [[nodiscard]] uint32_t get_x() const;
+
+    [[nodiscard]] uint32_t get_y() const;
+
+    [[nodiscard]] uint32_t get_dir() const;
 
     // корректная позиция + проходимо
     [[nodiscard]] bool is_valid() const;
@@ -24,11 +32,11 @@ struct Position {
     [[nodiscard]] Position counter_rotate() const;
 
     // применяет action на эту точку
-    [[nodiscard]] Position simulate_action(Action action) const;
+    [[nodiscard]] Position simulate_action(const Action &action) const;
+
+    friend bool operator==(const Position &lhs, const Position &rhs);
+
+    friend bool operator!=(const Position &lhs, const Position &rhs);
+
+    friend bool operator<(const Position &lhs, const Position &rhs);
 };
-
-bool operator==(const Position &lhs, const Position &rhs);
-
-bool operator!=(const Position &lhs, const Position &rhs);
-
-bool operator<(const Position &lhs, const Position &rhs);
