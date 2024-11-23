b4ee7fdf5e16b978aee14cb4020c9e9ef1deae79
Straple <hadayegor@gmail.com>
2024-11-23 11:46:40 +0300

    add time

---
+++ Solution/Objects/Basic/time.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Objects/Basic/assert.hpp>
+
+#include <iostream>
+#include <chrono>
+
+using TimePoint = std::chrono::steady_clock::time_point;
+using Milliseconds = std::chrono::milliseconds;
+using Nanoseconds = std::chrono::nanoseconds;
+
+TimePoint get_now();
+
+class Timer {
+    TimePoint start;
+
+public:
+    Timer();
+
+    [[nodiscard]] uint64_t get_ms() const;
+
+    [[nodiscard]] uint64_t get_ns() const;
+
+    void reset();
+};
+
+std::ostream &operator<<(std::ostream &output, const Timer &time);
