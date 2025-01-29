da1097c92260f5ae82b5a15c6903b1415cb39d55
straple <54230867+Straple@users.noreply.github.com>
2025-01-29 15:52:00 +0300

    Update environment.cpp

--- Solution/Objects/Environment/environment.cpp
+++ Solution/Objects/Environment/environment.cpp
@@ -109,8 +109,8 @@ void update_environment(SharedEnvironment &env) {
             FAILED_ASSERT("kek");
         } else if (get_map_type() == MapType::GAME) {
             // 5000 failed
-            // 7500 ok
-            ASSERT(env.num_of_agents <= 7499, "invalid num of agents");
+            // 7499 ok
+            ASSERT(env.num_of_agents <= 6000, "invalid num of agents");
         } else if (get_map_type() == MapType::WAREHOUSE) {
             ASSERT(env.curr_timestep < 5000, "invalid timestep");
             if (env.num_of_agents == 10'000) {
