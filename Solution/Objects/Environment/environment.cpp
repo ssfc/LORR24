5e3d6682094bc1f9571b6d6ce25f567c1a154158
straple <54230867+Straple@users.noreply.github.com>
2025-01-30 15:38:51 +0300

    Update environment.cpp

--- Solution/Objects/Environment/environment.cpp
+++ Solution/Objects/Environment/environment.cpp
@@ -149,6 +149,8 @@ void update_environment(SharedEnvironment &env) {
 
     if(get_map_type() == MapType::RANDOM) {
         ASSERT(tasks_size.size() <= 5, "invalid task size");
+    } else if(get_map_type() == MapType::GAME) {
+        ASSERT(tasks_size.size() <= 2, "invalid task size");
     } else{
         ASSERT(tasks_size == std::set<int>{2}, "invalid task size");
     }
