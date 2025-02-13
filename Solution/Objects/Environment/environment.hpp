f3b80dd25f59b6cccc3d27d847b01ea5b1456113
Straple <hadayegor@gmail.com>
2025-02-13 21:40:07 +0300

    add RawHeuristicMatrix

--- Solution/Objects/Environment/environment.hpp
+++ Solution/Objects/Environment/environment.hpp
@@ -1,20 +1,21 @@
 #pragma once
 
-#include <Objects/Environment/dynamic_heuristic_matrix.hpp>
 #include <Objects/Environment/dhmr.hpp>
+#include <Objects/Environment/dynamic_heuristic_matrix.hpp>
 #include <Objects/Environment/graph.hpp>
 #include <Objects/Environment/graph_guidance.hpp>
 #include <Objects/Environment/guidance_map.hpp>
 #include <Objects/Environment/heuristic_matrix.hpp>
+#include <Objects/Environment/info.hpp>
 #include <Objects/Environment/map.hpp>
 #include <Objects/Environment/operations.hpp>
 #include <Objects/Environment/operations_map.hpp>
+#include <Objects/Environment/raw_heuristic_matrix.hpp>
 #include <Objects/Environment/robot_handler.hpp>
-#include <Objects/Environment/info.hpp>
 #include <Objects/Environment/workload_map.hpp>
 
-#include <SharedEnv.h>
 #include <ActionModel.h>
+#include <SharedEnv.h>
 
 void init_environment(SharedEnvironment &env);
 
