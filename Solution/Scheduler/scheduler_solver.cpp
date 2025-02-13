ecb5a79c418e902c8b4b037bb6d94eca06830510
straple <54230867+Straple@users.noreply.github.com>
2025-02-13 09:10:25 +0300

    use dist_to_target^2 + task_dist

--- Solution/Scheduler/scheduler_solver.cpp
+++ Solution/Scheduler/scheduler_solver.cpp
@@ -57,8 +57,10 @@ uint64_t SchedulerSolver::get_dist(uint32_t r, uint32_t t) const {
 
     uint32_t source = get_robots_handler().get_robot(r).node;
     uint64_t dist_to_target = get_hm().get(source, task_target[t]);
-    // uint64_t dist = dist_to_target * dist_to_target + dist_dp[t];
     uint64_t dist = dist_to_target + dist_dp[t];
+    if(get_test_type() == TestType::GAME || get_test_type() == TestType::WAREHOUSE || get_test_type() == TestType::SORTATION){
+        dist = dist_to_target * dist_to_target + dist_dp[t];
+    }
     return dist;
 }
 
