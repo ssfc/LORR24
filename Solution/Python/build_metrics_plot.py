de478a6c6c9274223afe637b74a5047055277130
Straple <hadayegor@gmail.com>
2025-03-17 12:24:20 +0300

    improve build metrics plot

--- Solution/Python/build_metrics_plot.py
+++ Solution/Python/build_metrics_plot.py
@@ -3,6 +3,7 @@ import numpy as np
 import sys
 import matplotlib.colors
 import pandas as pd
+from PIL import Image
 
 if __name__ == '__main__':
     df = pd.read_csv('../../Data_random/total_metrics.csv', sep=';')
@@ -13,25 +14,34 @@ if __name__ == '__main__':
 
     grouped = df.groupby('planner algo')
 
-    fig, axes = plt.subplots(2, 1, figsize=(10, 10))
+    fig, axes = plt.subplots(3, 1, figsize=(10, 10))
     images = []
 
     ax = axes[0]
+    ax.imshow(np.asarray(Image.open('random.png')))
+    ax.title.set_text('random-32-32-20\nSize: 32x32\n|V|=819')
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax = axes[1]
 
     plan_algos = ['pibt', 'pibt_tf', 'epibt', 'epibt_lns', 'pepibt_lns', 'wppl']
     plan_algos_name = ['PIBT', 'PIBT+traffic flow', 'EPIBT', 'EPIBT+LNS', 'Parallel EPIBT+LNS', 'WPPL']
+    markers = ['o', 'v', 's', 'p', '*', 'x']
 
     for i in range(len(plan_algos)):
         df = grouped.get_group(plan_algos[i])
-        images.append(ax.plot(df['agents num'], df['throughput'], label=plan_algos_name[i]))
+        images.append(ax.plot(df['agents num'], df['throughput'], label=plan_algos_name[i], marker=markers[i]))
         ax.set_ylabel('throughput')
+        ax.grid(True)
 
-    ax = axes[1]
+    ax = axes[2]
     for i in range(len(plan_algos)):
         df = grouped.get_group(plan_algos[i])
-        images.append(ax.plot(df['agents num'], df['avg step time'], label=plan_algos_name[i]))
+        images.append(ax.plot(df['agents num'], df['avg step time'], label=plan_algos_name[i], marker=markers[i]))
         ax.set_yscale('log')
         ax.set_ylabel('avg step time')
+        ax.grid(True)
 
     fig.supxlabel('agents num')
 
@@ -41,6 +51,6 @@ if __name__ == '__main__':
     while len(lines) > len(plan_algos):
         lines.pop(-1)
         labels.pop(-1)
-    fig.legend(lines, labels, loc='upper center', ncol=4)
+    fig.legend(lines, labels, loc='center right', ncol=1)
 
     plt.show()
