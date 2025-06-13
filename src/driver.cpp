89ee4d52884cc3194bdcdda1219e24122a5d88bc
SSFC <sortsimilar@gmail.com>
2025-06-14 01:41:23 +0800

    // ./bin/lifelong -i ./2024main-instance/MainRoundEvaluationInstances/warehouse.domain/WAREHOUSE.json -o test.json -s 1000 -t 300 -p 1800000 --pa 'epibt(4)' --gg enable --sa greedy

--- src/driver.cpp
+++ src/driver.cpp
@@ -262,6 +262,8 @@ int main(int argc, char **argv) {
 // Run on ubuntu platform:
 // ./bin/lifelong -i ./example_problems_hse/random.domain/random_32_32_20_400.json -o test.json -s 1000 -t 300 -p 1800000 --pa 'epibt(4)' --gg enable --sa greedy
 // ./bin/lifelong -i ./2024main-instance/MainRoundEvaluationInstances/random.domain/RANDOM-01.json -o test.json -s 1000 -t 300 -p 1800000 --pa 'epibt(4)' --gg enable --sa greedy
+// ./bin/lifelong -i ./2024main-instance/MainRoundEvaluationInstances/random.domain/RANDOM-01.json -o test.json -s 1000 -t 300 -p 1800000 --pa 'epibt(4)' --gg enable --sa greedy
+// ./bin/lifelong -i ./2024main-instance/MainRoundEvaluationInstances/warehouse.domain/WAREHOUSE.json -o test.json -s 1000 -t 300 -p 1800000 --pa 'epibt(4)' --gg enable --sa greedy
 
 
 
