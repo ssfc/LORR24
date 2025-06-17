1d59ff689c3d470acdeaaa5b5869ffb8134e8fcb
SSFC <sortsimilar@gmail.com>
2025-06-17 19:34:30 +0800

    // 返回一个静态局部变量 type（类型为 PlannerType）的引用。

--- Solution/Objects/Environment/info.cpp
+++ Solution/Objects/Environment/info.cpp
@@ -12,16 +12,19 @@ MapType &get_map_type() {
     return type;
 }
 
+// 返回一个静态局部变量 type（类型为 PlannerType）的引用。
 PlannerType &get_planner_type() {
     static PlannerType type = PlannerType::EPIBT;
     return type;
 }
 
+// 提供一个全局可访问、可修改的 GraphGuidanceType 类型变量的引用，并确保只有这一份变量被使用。
 GraphGuidanceType &get_graph_guidance_type() {
     static GraphGuidanceType type = GraphGuidanceType::ENABLE;
     return type;
 }
 
+// 提供一个全局唯一的、可读可写的 SchedulerType 类型变量，并通过函数接口进行访问和管理。
 SchedulerType &get_scheduler_type() {
     static SchedulerType type = SchedulerType::GREEDY;
     return type;
@@ -33,6 +36,7 @@ uint32_t &get_epibt_operation_depth() {
     return depth;
 }
 
+// 全局管理和访问“禁用的代理（agents）数量”，并允许你随时获取或修改这个数值。
 uint32_t &get_disable_agents() {
     static uint32_t disable_agents = 0;
     return disable_agents;
