4e330c3223c6ef997112d58d73f4290ac94ff7f9
SSFC <26841985+ssfc@users.noreply.github.com>
2025-11-15 21:33:53 +0800

    // 🧩 4. 构建 task_metric / task_target（任务内部代价）

--- Solution/Scheduler/scheduler_solver.cpp
+++ Solution/Scheduler/scheduler_solver.cpp
@@ -220,6 +220,7 @@ void SchedulerSolver::update() {
     }
 
     // build task_metric, task_target
+    // 🧩 4. 构建 task_metric / task_target（任务内部代价）
     {
         ETimer timer;
         for (uint32_t t: free_tasks) {
