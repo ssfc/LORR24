b7e5037372be9d53e1015f01e2603e398323b011
SSFC <sortsimilar@gmail.com>
2025-11-18 15:41:31 +0800

    struct AgentTask

--- Solution/Scheduler/scheduler_solver.hpp
+++ Solution/Scheduler/scheduler_solver.hpp
@@ -22,6 +22,27 @@ struct SchedulerSolver {
 
     std::vector<uint32_t> free_tasks;
 
+
+    struct AgentTask
+    {
+        int task_id = -1;
+        int min_task_dist = -1;
+        double jam_when_assign = -1; // 当该任务分配时, agent前往该任务的拥堵系数
+        double task_heuristic = -1; // 启发值必然是double, 因为即使sum jam weight是int, 乘以系数后还是要变成double
+        int dist_agent_pickup = -1; // 从agent当前位置到pickup点的启发式距离
+        int dist_pickup_delivery = -1; // task pickup点到delivery点的启发式距离
+        int assign_moment = -1;
+        int complete_moment = -1;
+    };
+    vector<AgentTask> agent_task;
+
+    int numTaskFinished = 0;
+    int total_min_span = 0; // 已完成任务的理论完成时间下界之和
+    int total_real_duration = 0; // 已完成任务的实际完成时间之和
+    double total_jam = 0;
+    double jam_coefficient = 1; // 在多大程度上考虑拥堵
+
+
     // dp[r] = отсортированный вектор (dist, task_id)
     std::vector<std::vector<std::pair<uint32_t, uint32_t>>> dp;
 
