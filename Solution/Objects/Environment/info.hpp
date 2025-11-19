72baede37afcc099ae4d7760367dbdff263cfe3b
SSFC <sortsimilar@gmail.com>
2025-11-19 09:36:16 +0800

    GREEDY, // HSE的greedy算法     HUNGARIAN, // HSE的匈牙利算法     DEFAULT_GREEDY, // 默认的greedys算法     adaptive_jam_curr_pickup_intersect_curr_goal, // 数交叉点     adaptive_jam_task_pickup_region_count_current, // 数区域内agent数量

--- Solution/Objects/Environment/info.hpp
+++ Solution/Objects/Environment/info.hpp
@@ -35,11 +35,11 @@ enum class GraphGuidanceType {
 GraphGuidanceType &get_graph_guidance_type();
 
 enum class SchedulerType {
-    GREEDY,
-    HUNGARIAN,
-    DEFAULT_GREEDY,
-    adaptive_jam_curr_pickup_intersect_curr_goal,
-    adaptive_jam_task_pickup_region_count_current,
+    GREEDY, // HSE的greedy算法
+    HUNGARIAN, // HSE的匈牙利算法
+    DEFAULT_GREEDY, // 默认的greedys算法
+    adaptive_jam_curr_pickup_intersect_curr_goal, // 数交叉点
+    adaptive_jam_task_pickup_region_count_current, // 数区域内agent数量
 };
 
 SchedulerType &get_scheduler_type();
