59f3c8d2891655d09805c0cd64a6e5ce299b7c26
SSFC <sortsimilar@gmail.com>
2025-11-21 20:11:42 +0800

    if (solver.agent_task[element].min_task_dist > solver.agent_task[element].complete_moment - solver.agent_task[element].assign_moment) {

--- Solution/Scheduler/scheduler.cpp
+++ Solution/Scheduler/scheduler.cpp
@@ -69,10 +69,17 @@ void MyScheduler::solver_schedule(TimePoint end_time, std::vector<int> &proposed
             cout << "Agent " << element << " complete task " << solver.agent_task[element].task_id
                  << " minDist " << solver.agent_task[element].min_task_dist
                  << " estimate time taken " << solver.agent_task[element].task_heuristic
-                 << " complete moment " << solver.agent_task[element].complete_moment
-                 << " assign moment " << solver.agent_task[element].assign_moment
+                 // << " complete moment " << solver.agent_task[element].complete_moment
+                 // << " assign moment " << solver.agent_task[element].assign_moment
                  << " real time taken " << solver.agent_task[element].complete_moment - solver.agent_task[element].assign_moment
                  << " jam " << solver.agent_task[element].jam_when_assign << endl;
+
+            if (solver.agent_task[element].min_task_dist > solver.agent_task[element].complete_moment - solver.agent_task[element].assign_moment) {
+                std::cerr << "Error: minDist (" << solver.agent_task[element].min_task_dist
+                     << ") > real time taken (" << solver.agent_task[element].complete_moment - solver.agent_task[element].assign_moment
+                      << ") for agent " << element << std::endl;
+                std::terminate();  // 或 std::abort();
+            }
         }
 
         solver.agent_task[element].task_id = -1;
